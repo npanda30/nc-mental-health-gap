658b32525bb4d9216e6a9672cfee6f2bdf44d1fc
Neha Panda <nehap2@unc.edu>
2026-06-09 08:13:33 -0400

    update 05_morans_i.py with results

--- scripts/05_morans_i.py
+++ scripts/05_morans_i.py
@@ -10,3 +10,18 @@ nc = nc[nc['STATEFP']=='37'][['GEOID', 'NAME', 'geometry']].copy()
 # Note here the FIPS column is titled GEOID
 
 # Merge on the nc[GEOID] and master[fips] columns
+master['fips'] = master['fips'].astype(str).str.zfill(5)
+merged = nc.merge(master, left_on='GEOID', right_on='fips', how='left')
+
+print(merged[['GEOID', 'county', 'fips', 'gap_index', 'geometry']].head())
+print(merged.shape)
+
+from libpysal.weights import Queen
+mergedwq = Queen.from_dataframe(merged, use_index=False)
+print(mergedwq)
+print(mergedwq.n)
+
+mi = esda.Moran(merged['gap_index'], mergedwq)
+
+print(mi.I)
+print(mi.p_sim)
